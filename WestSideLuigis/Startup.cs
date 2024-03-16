e5adb56edfc20b4ade48a2f88a48c860932ba76b
guduudow <bbo3063@gmail.com>
2024-03-16 13:16:20 -0400

    Created Solution in Visual Studio. Nothing else has been made or added beyond that.

---
+++ WestSideLuigis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WestSideLuigis.Startup))]
+namespace WestSideLuigis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
